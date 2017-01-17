e7a99eba8d0c186014b6d27ec37fe2da2f35a603
lucs <lucasnvcarvalho@gmail.com>
2017-01-17 18:04:55 -0200

    initial module code

---
+++ register_types.cpp
@@ -0,0 +1,14 @@
+/* register_types.cpp */
+
+#include "register_types.h"
+#include "object_type_db.h"
+#include "print_string.h"
+
+void register_chipmunk_types()
+{
+    print_line("first test");
+}
+
+void unregister_chipmunk_types()
+{
+}
---
+++ register_types.h
@@ -0,0 +1,4 @@
+/* register_types.h */
+
+void register_chipmunk_types();
+void unregister_chipmunk_types();
